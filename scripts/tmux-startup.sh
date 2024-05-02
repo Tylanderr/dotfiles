4c1462f99982c8f838c71b5a32b7a751253bafd1
Tylander732 <tyler.harding.sde@gmail.com>
2024-05-02 11:42:32 -0700

    Commit

--- scripts/tmux-startup.sh
+++ scripts/tmux-startup.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# Uncomment for a single window to start up
+# tmux new-session -s Dev -n nvim
+
+# Uncomment for multiple windows to startup
 tmux new-session -s Dev -n nvim -d
 tmux new-window -t Dev:2 -n term
 tmux select-window -t Dev:1
