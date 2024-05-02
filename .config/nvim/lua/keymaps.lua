d91c47c6ba61ad314469c5a3f7a6b6717c208d64
Tylander732 <tyler.harding.sde@gmail.com>
2024-05-02 11:09:31 -0700

    Updated netrw view to be <leader>tr

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>tr', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>qu', vim.cmd.q)
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
