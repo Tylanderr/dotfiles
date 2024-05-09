fcc10cb08ba276aee62e4d4953becd318f35b95c
Tylander732 <tyler.harding.sde@gmail.com>
2024-05-08 20:15:20 -0700

    Added lazygit plugin and configuration

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -12,36 +12,22 @@ require('lazy').setup({
   --  This is equivalent to:
   --    require('Comment').setup({})
   { 'numToStr/Comment.nvim', opts = {} },
-
   require 'tyler/plugins/telescope',
-
   require 'tyler/plugins/java',
-
   require 'tyler/plugins/lspconfig',
-
   require 'tyler/plugins/cmp',
-
   require 'tyler/plugins/tokyonight',
-
   require 'tyler/plugins/todo-comments',
-
   require 'tyler/plugins/mini',
-
   require 'tyler/plugins/treesitter',
-
   require 'tyler/plugins/harpoon',
-
   require 'tyler/plugins/fugitive',
-
   require 'tyler/plugins/undotree',
-
   require 'tyler/plugins/autopairs',
-
   require 'tyler/plugins/refactoring',
-
   require 'tyler/plugins/trouble',
-
-  require 'tyler/plugins/noice'
+  require 'tyler/plugins/noice',
+  require 'tyler/plugins/lazygit'
 })
 
 require('lspconfig').jdtls.setup {}
