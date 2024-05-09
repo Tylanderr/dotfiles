20078fa7cbfa20912c34f853e87f223acd012d99
Tylander732 <tyler.harding.sde@gmail.com>
2024-05-08 20:56:55 -0700

    staging the rest of the change

--- .config/nvim/lua/tyler/plugins/oil.lua
+++ .config/nvim/lua/tyler/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   'stevearc/oil.nvim',
   opts = {
     default_file_explorer = true,
+    view_options = {
+      show_hidden = true
+    }
   },
   vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 }
