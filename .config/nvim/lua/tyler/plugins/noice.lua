7ef116be73c388ed07ea7240e29bc55da5f03ef4
Tylander <tyler.harding.sde@gmail.com>
2024-05-15 12:46:59 -0700

    Update noice.lua

--- .config/nvim/lua/tyler/plugins/noice.lua
+++ .config/nvim/lua/tyler/plugins/noice.lua
@@ -2,6 +2,21 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+    routes = {
+      {
+        filter = {
+          event = 'msg_show',
+          any = {
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
+            { find = '%d fewer lines' },
+            { find = '%d more lines' },
+          },
+        },
+        opts = { skip = true },
+      }
+    },
     messages = {
       enabled = true
     },
@@ -25,7 +40,7 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    {"rcarriga/nvim-notify", opts = { stages = "static", render = "minimal", minimum_width = 10} },
+    { "rcarriga/nvim-notify", opts = { stages = "static", render = "minimal", minimum_width = 10 } },
   },
   vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" }),
   vim.keymap.set("n", "<leader>nt", "<cmd>Noice telescope<CR>", { desc = "Show noice messages in telescope" }),
