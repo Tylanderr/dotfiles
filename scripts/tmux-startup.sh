53f8ef3da1cd8d4258eb1cd76a27feb5a9d2b625
Tylander <tyler.harding.sde@gmail.com>
2024-05-30 08:45:11 -0700

    Delete scripts/tmux-startup.sh

--- scripts/tmux-startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Uncomment for a single window to start up
-tmux new-session -s Dev -n nvim
-
-# Uncomment for multiple windows to startup
-#tmux new-session -s Dev -n nvim -d
-#tmux new-window -t Dev:2 -n term
-#tmux select-window -t Dev:1
-#tmux -2 attach-session -t Dev
