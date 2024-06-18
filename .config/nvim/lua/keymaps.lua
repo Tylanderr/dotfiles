b3cc852811b24d424de0d48196f7e9894b0a6c84
Tylander <tyler.harding.sde@gmail.com>
2024-06-18 08:38:43 -0700

    Update keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,29 +1,28 @@
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<CR>")
 
+vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "z", "zz")
+
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>qu", vim.cmd.q)
-vim.keymap.set("n", "<leader>QU", "<cmd>qa!<CR>", { silent = true })
+vim.keymap.set("n", "<leader>qu", "<cmd>q<CR>")
+vim.keymap.set("n", "<leader>QU", "<cmd>qa!<CR>")
 
 vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>nh", "<cmd>noh<CR>")
 vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<CR>")
 
