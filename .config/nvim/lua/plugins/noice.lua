2e06b251e86306f28fd2617592e855eb8927e113
Tylander <tyler.harding.sde@gmail.com>
2024-06-26 21:03:13 -0700

    added cmp to noice

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -27,6 +27,11 @@ return {
       enabled = false
     },
     lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
       progress = {
         enabled = false
       }
