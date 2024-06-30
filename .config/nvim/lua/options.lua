ae1d4127e6625c62028a5d46c395e92067a24e26
Tylander <tyler.harding.sde@gmail.com>
2024-06-29 21:17:13 -0700

    cursor block, updated scrolloff value

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,4 +1,4 @@
--- vim.opt.guicursor = ""
+vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -23,7 +23,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append '@-@'
 
