99b31c0b770c7d6088b8366035d396516c2feb36
Tylander <tyler.harding.sde@gmail.com>
2024-07-08 13:10:28 -0700

    Update lualine.lua

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,22 @@ return {
     require('lualine').setup {
       options = {
         theme = 'tokyonight'
+      },
+      sections = {
+        lualine_b = {
+          'branch'
+        },
+        lualine_c = {
+          {
+            'filename',
+            path = 1
+          }
+        },
+        lualine_x = {
+          {
+            'encoding'
+          }
+        }
       }
     }
   end
