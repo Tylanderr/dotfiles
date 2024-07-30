098f2256cbc22d97f41220cae0ade168d3669f9c
Tylander <tyler.harding.sde@gmail.com>
2024-07-30 06:17:32 -0700

    Update lspconfig.lua

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -33,8 +33,6 @@ return {
           map('qc', '<cmd>cclose<CR>', '[Q]uick Reference [C]lose')
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
           map('<leader>vd', function() vim.diagnostic.open_float() end, 'Open diagnostics float')
