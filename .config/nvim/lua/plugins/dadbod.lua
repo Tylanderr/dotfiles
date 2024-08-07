cabda21f5f2cc13a3e640fdd2c7057e3c7376c52
Tylander <tyler.harding.sde@gmail.com>
2024-08-06 20:45:48 -0700

    removed sql default keymaps

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -13,6 +13,7 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.omni_sql_no_default_maps = 1
     vim.keymap.set("n", "<leader>db", "<cmd>DBUIToggle<CR>")
   end,
 }
