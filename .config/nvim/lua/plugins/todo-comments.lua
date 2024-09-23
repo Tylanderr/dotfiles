1efc3a55115db0c034d62d08a06f2be25c587b7e
Tyler Harding <tyler.harding.sde@gmail.com>
2024-09-23 10:53:07 -0700

    Update todo-comments.lua

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,3 +1,6 @@
 return {
-  { 'folke/todo-comments.nvim', event = 'VeryLazy', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim', event = 'VeryLazy', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false },
+    vim.keymap.set("n", "<leader>td", "<cmd>TodoQuickFix<CR>")
+  },
 }
