ef787c2e77c313547d8fb47862dba5e8bead1746
Tyler Harding <tyler.harding.sde@gmail.com>
2024-09-23 07:27:35 -0700

    Update oil.lua

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -17,21 +17,7 @@ return {
 				["<C-c>"] = false,
 				["q"] = "actions.close",
 			},
-			float = {
-				padding = 2,
-
-				max_width = 225,
-				max_height = 60,
-				border = "rounded",
-				win_options = {
-					winblend = 0,
-				},
-				override = function(conf)
-					return conf
-				end,
-			},
 		})
 		vim.keymap.set("n", "-", "<cmd>Oil<CR>")
-		vim.keymap.set("n", "<leader>-", require("oil").open_float)
 	end,
 }
