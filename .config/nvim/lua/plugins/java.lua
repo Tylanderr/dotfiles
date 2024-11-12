d3d182e006af84cb96aa12ff8a40924801da7b5e
Tyler Harding <tyler.harding.sde@gmail.com>
2024-11-12 06:46:40 -0800

    Update java.lua

--- .config/nvim/lua/plugins/java.lua
+++ .config/nvim/lua/plugins/java.lua
@@ -2,20 +2,7 @@ return {
   'nvim-java/nvim-java',
   ft = "java",
   config = function()
-    require('java').setup({
-      java_test = {
-        enable = false
-      },
-      java_debug_adapter = {
-        enable = false
-      },
-      spring_boot_tools = {
-        enable = false
-      },
-      notifications = {
-        dap = false
-      }
-    })
+    require('java').setup({})
     require('lspconfig').jdtls.setup({})
   end,
 }
