9789c27d63fc9b471cc192a98a28cad21b359c14
Tyler Harding <tyler.harding.sde@gmail.com>
2025-06-11 08:29:58 -0700

    Update lazy-plugins.lua

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -1,18 +1,15 @@
 require('lazy').setup({
   { 'tpope/vim-sleuth',      event = "VeryLazy", },
-  { 'numToStr/Comment.nvim', event = "VeryLazy", opts = {} },
   { 'windwp/nvim-autopairs', event = "VeryLazy", opts = {} },
 
   require 'plugins/intro',
   require 'plugins/telescope',
   require 'plugins/lspconfig',
   require 'plugins/cmp',
-  -- require 'plugins/debug',
   require 'plugins/tokyonight',
   require 'plugins/todo-comments',
   require 'plugins/mini',
   require 'plugins/treesitter',
-  require 'plugins/indent',
   require 'plugins/harpoon',
   require 'plugins/fugitive',
   require 'plugins/undotree',
