152cb4d517ee5f35bcb00494823316611752bb8e
Tyler Harding <tyler.harding.sde@gmail.com>
2025-06-12 09:35:53 -0700

    Update snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -21,14 +21,10 @@ return {
     words = { enabled = false },
   },
   keys = {
-    -- Top Pickers & Explorer
     { "<leader>fc",       function() Snacks.picker.grep() end,        desc = "Grep" },
-    -- find
     { "<leader><leader>", function() Snacks.picker.buffers() end,     desc = "Buffers" },
     { "<leader>ff",       function() Snacks.picker.files() end,       desc = "Find Files" },
-    -- Grep
     { "<leader>fw",       function() Snacks.picker.grep_word() end,   desc = "Visual selection or word", mode = { "n", "x" } },
-    -- search
     { "<leader>fd",       function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
     { "<leader>vh",       function() Snacks.picker.help() end,        desc = "Help Pages" },
     { "<leader>fk",       function() Snacks.picker.keymaps() end,     desc = "Keymaps" },
