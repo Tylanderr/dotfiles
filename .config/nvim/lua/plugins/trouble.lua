1ef548765c2527811010d5ccb8790525764ed191
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-08 10:54:10 -0700

    Create trouble.lua

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/trouble.nvim",
+  config = function()
+    require("trouble").setup({})
+
+    vim.keymap.set("n", "<leader>qf", function()
+      require("trouble").toggle("diagnostics")
+    end)
+
+    vim.keymap.set("n", "qn", function()
+      require("trouble").next({skip_groups = true, jump = true})
+    end)
+
+    vim.keymap.set("n", "qp", function()
+      require("trouble").prev({skip_groups = true, jump = true})
+    end)
+  end
+}
