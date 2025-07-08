d71842e683d67703d7b652392526e6de6c4ec959
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-08 11:19:38 -0700

    Update trouble.lua

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -4,8 +4,11 @@ return {
   config = function()
     require("trouble").setup({})
 
-    vim.keymap.set("n", "<leader>qf", function()
-      require("trouble").toggle("diagnostics")
+    vim.keymap.set("n", "<leader>qf", "<cmd>Trouble diagnostics filter.severity=vim.diagnostic.severity.ERROR<CR>")
+    vim.keymap.set("n", "<leader>qt", "<cmd>Trouble diagnostics toggle<CR>")
+
+    vim.keymap.set("n", "qf", function()
+      require("trouble").jump({skip_groups = true})
     end)
 
     vim.keymap.set("n", "qn", function()
@@ -15,5 +18,6 @@ return {
     vim.keymap.set("n", "qp", function()
       require("trouble").prev({skip_groups = true, jump = true})
     end)
+
   end
 }
