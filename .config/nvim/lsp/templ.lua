3292c436b69fd66f5a496ee60418a338c7e72039
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-23 20:13:09 -0700

    fixed templ commenting issue

--- .config/nvim/lsp/templ.lua
+++ .config/nvim/lsp/templ.lua
@@ -2,4 +2,12 @@ return {
   cmd = { 'templ', 'lsp' },
   filetypes = { 'templ' },
   root_markers = { 'go.work', 'go.mod', '.git' },
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "templ",
+    callback = function()
+      vim.bo.commentstring = "// %s"
+    end
+  })
 }
+
