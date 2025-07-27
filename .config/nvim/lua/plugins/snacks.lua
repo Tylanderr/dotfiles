e2f71ba8d04a84921883a64bcb039267c33917d5
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-27 07:04:04 -0700

    updated snacks picker configurations

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,21 +1,67 @@
+local function palette_layout(height, width)
+  height = height or 0.4
+  width = width or 0.6
+  return {
+    preview = false,
+    layout = {
+      backdrop = false,
+      width = 0.5,
+      min_width = 80,
+      height = 0.6,
+      min_height = 3,
+      box = "vertical",
+      border = "rounded",
+      title = "{title}",
+      title_pos = "center",
+      { win = "input",   height = 1,          border = "bottom" },
+      { win = "list",    border = "none" },
+      { win = "preview", title = "{preview}", height = 0.4,     border = "top" },
+    },
+  }
+end
+
+local function files_layout(preview_width, height, width)
+  preview_width = preview_width or 0.6
+  height = height or 0.8
+  width = width or 0.8
+  return {
+    layout = {
+      box = 'horizontal',
+      width = width,
+      min_width = 120,
+      height = height,
+      {
+        box = 'vertical',
+        border = 'rounded',
+        title = '{title} {live} {flags}',
+        { win = 'input', height = 1,     border = 'bottom' },
+        { win = 'list',  border = 'none' },
+      },
+      { win = 'preview', title = '{preview}', border = 'rounded', width = preview_width },
+    },
+  }
+end
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     picker = {
       enabled = true,
       sources = {
-        files = {
-          layout = { preset = "select" },
-          hidden = true
-        },
+        files = { layout = palette_layout(), hidden = true },
+        grep = { layout = files_layout() },
+        buffers = { layout = files_layout() },
+        grep_word = { layout = files_layout() },
+        diagnostics = { layout = files_layout() },
+        lsp_references = { layout = files_layout() },
+        help = { layout = { preset = "ivy" } },
+        keymaps = { layout = { preset = "ivy" } },
+        undo = { layout = { preset = "ivy" } },
       },
     },
     quickfile = { enabled = true },
