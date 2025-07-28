34c7d3fd3f16de9fecf5466024db28d3600bd5aa
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-28 07:33:00 -0700

    Update oil.lua

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,26 +1,27 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = {
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
-	},
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			skip_confirm_for_simple_edits = true,
-			view_options = {
-				show_hidden = true,
-			},
-			keymaps = {
-				-- overwrite default keymaps with custom functions
-				["<C-s>"] = false,
-				["<C-h>"] = false,
-				["<C-j>"] = false,
-				["<C-k>"] = false,
-				["<C-l>"] = false,
-				["<C-c>"] = false,
-				["q"] = "actions.close",
-			},
-		})
-		vim.keymap.set("n", "-", "<cmd>Oil<CR>")
-	end,
+  "stevearc/oil.nvim",
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        -- overwrite default keymaps with custom functions
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<C-l>"] = false,
+        ["<C-c>"] = false,
+        ["q"] = "actions.close",
+      },
+    })
+    vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+  end,
 }
+-- vim: ts=2 sts=2 sw=2 et
